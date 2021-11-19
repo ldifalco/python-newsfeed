5ae918ab84d0dfc6b427266ed0d866cb1a100ef8
Lucas <luke.difalco52@gmail.com>
2021-11-19 01:53:21 -0500

    created signup api route, added error handling, added app sessions, created routes for login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
@@ -7,6 +7,7 @@ def create_app(test_config=None):
     #set up app config
     app = Flask(__name__, static_url_path= '/')
     app.url_map.strict_slashes = False
+    app.register_blueprint(api)
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     )
--- app/models/User.py
+++ app/models/User.py
@@ -12,6 +12,11 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+    def verify_password(self, password):
+      return bcrypt.checkpw(
+        password.encode('utf-8'),
+        self.password.encode('utf-8')
+  )
 
     @validates('password')
     def validate_password(self, key, password):
